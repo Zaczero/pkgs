f8215567224dea1324581b7d725ed8bc662b7954
Kamil Monicz <kamil@monicz.dev>
2024-11-17 06:50:49 +0000

    Quote nix-shell run arg, Configure flake8-type-checking

--- packages/githead/githead/__init__.py
+++ packages/githead/githead/__init__.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
-from os import PathLike
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from os import PathLike
 
 
 def githead(git_dir: PathLike[str] | str = '.git') -> str:
